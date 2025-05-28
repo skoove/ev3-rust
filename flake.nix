5fc2e842f23685a504b8bc5a480d38ff61b7263c
Zie <53106860+skoove@users.noreply.github.com>
2025-05-29 07:59:00 +1000

    wip

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, fenix }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    fe_pkgs = fenix.packages.${system};
+
+    librarys = with pkgs; [
+    ];
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        (fe_pkgs.complete.withComponents [
+          "cargo"
+          "clippy"
+          "rust-src"
+          "rustc"
+          "rustfmt"
+          "rust-analyzer"
+        ])
+        clang
+        lld
+        pkg-config
+        rustup
+        buildPackages.gccCrossMuslArmv6
+      ] ++ librarys;
+
+      LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath librarys}";
+    };
+  };
+}
