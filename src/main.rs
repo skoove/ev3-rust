af98969e4aa7c93532f73522d420dac713595263
Zie <53106860+skoove@users.noreply.github.com>
2025-06-18 21:51:12 +1000

    skibidfi

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::{thread::sleep, time::Duration};
+
+use ev3dev_lang_rust::{sound, Ev3Error};
+
+fn main() -> Result<(), Ev3Error> {
+    sound::play("weezer.wav")?.wait();
+
+    sleep(Duration::from_millis(250));
+
+    sound::speak("i wish for death")?.wait();
+
+    sleep(Duration::from_millis(500));
+
+    sound::speak("kill me");
+
+    Ok(())
 }
